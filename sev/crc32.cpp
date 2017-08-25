109afd708e28ee43fda16251b33d01ff4ba74d28
kaetemi <kaetemi@gmail.com>
2017-08-25 19:05:50 +0800

    Interesting cases

--- sev/crc32.cpp
+++ sev/crc32.cpp
@@ -38,13 +38,38 @@ SEV_LIB crc32_t crc32(const void *buffer, size_t length)
 
 namespace /* anonymous */ {
 
+
 namespace t̶̡̨̤̭̙̘͇̲̺̫͎͔̲̟̕͠é҉͉̼̜̮̹̝͡ś̷̷̨̛̻̮̤̣̱̹͈̞͕͔̤̘̤ͅt҉̰͚̘͖͙̩̮̼̤͕͙̳͕̞͔̘͜ͅ {
 
 template<crc32_t v>
 crc32_t getV() { return v; }
 
+struct power_2_
+{
+};
+
+template<typename T>
+constexpr static auto operator*(T && v, const power_2_ &v)
+{
+	return v * v;
+}
+
+constexpr power_2_ power_2;
+
+#define ² * power_2
+
+void test_power_2()
+{
+	constexpr int x = 8;
+	static_assert(x ² == 64, "Neat");
+	static_assert(4 ² == 16, "Neat");
+}
+
 void test()
 {
+#define € sdf
+#define µ dsf
+	
 	// Compilation must succeed
 	getV<300>();
 	getV<crc32("What")>();
